9e53d0bba28b6e6faa55415b153cf492d97d188e
suxiaoping <1825082094@qq.com>
2022-11-23 21:47:28 +0800

    master commit2

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -7,5 +7,6 @@ public class Main{
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("master test");
     }
 }
