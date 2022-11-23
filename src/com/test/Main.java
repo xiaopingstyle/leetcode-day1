6db6238bbb94df4c3c5e5936893d5e10f80d0234
suxiaoping <1825082094@qq.com>
2022-11-23 21:26:45 +0800

    third commit

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -3,7 +3,8 @@ package com.test;
 public class Main{
 
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("hello world");
+        System.out.println("hello world");
         System.out.println("hello world");
     }
 }
